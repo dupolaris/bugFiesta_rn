7e3f439e1413643b4886ee65f0c8f09d0e6de0c0
Muhammad Dawood <73556674+dupolaris@users.noreply.github.com>
2023-12-10 05:53:35 +0500

    Initial Hello Commit
    hey buggy

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text, SafeAreaView} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hello Awesome01</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default App;
